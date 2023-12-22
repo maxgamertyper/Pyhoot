2cae9e4fb621e312398fd9a7c9fe63d73f0531fc
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2023-12-22 00:51:06 -0700

    Update base_example.py

--- examples/base_example.py
+++ examples/base_example.py
@@ -1,1 +1,17 @@
+from Pyhoot.Client import Client
+import time
 
+gamepin=str(input("Enter Gamepin: "))
+
+bot=Client(gamepin, name="Max")
+
+@bot.event_listener(type="question_started")
+def question(data):
+    bot.random_answer()
+
+#more listeners in here
+
+
+#bot function calls go either in listeners or after bot.start()
+bot.start()
+bot.join()
