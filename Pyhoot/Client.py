ef3f1f4461949af3b831af61ef30d163ad4fa707
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2023-12-23 22:07:03 -0700

    Update Client.py

--- Pyhoot/Client.py
+++ Pyhoot/Client.py
@@ -13,6 +13,8 @@ class Client():
         self.data = {}
         self.sent = {}
         self.log=[]
+        self.latest_listener_call=None
+        self.previous_send={}
         self.message_count = 1
         self.WebSocket = None
         self.WebChannels={ "META_HANDSHAKE": "/meta/handshake", "META_CONNECT": "/meta/connect", "META_DISCONNECT": "/meta/disconnect", "SERVICE_CONTROLLER": "/service/controller", "SERVICE_PLAYER": "/service/player" }
@@ -28,7 +30,10 @@ class Client():
         self.quizuuid=quizuuid
         self.questionindex=1
         self.listening_functions={}
-        self.listening_types=["pinged","handshake_1","handshake_2","avatar_updated", "disconnected", "question_started", "question_ended", "quiz_started", "quiz_ended", "unknown_message"]
+        self.listening_types=["pinged","handshake_1","handshake_2","avatar_updated", "disconnected", "question_started", "question_ended", "question_awaited", "quiz_started", "quiz_ended", "unknown_message","joined"]
+        self.question_data=None
+        self.disconnected=False
+        self.quiz_data={}
 
         a=Token(gamepin,self.UserAgent)
         self.token=a["Token"]
@@ -103,14 +108,15 @@ class Client():
     
     #used for reciving messages
     def MessageReceiver(self, msg):
+        self.previous_send=self.data
         self.data = JSON.loads(msg)[0]
-        print(self.data,"\n in \n")
         self.log.append(JSON.loads(msg))
         self.MessageHandler()
 
     #used for handling information on messages
     def MessageHandler(self):
         func=None
+        prev_func=None
         data=self.data.get("data",{})
         
         error = data.get("error") if data.get("error") is not None else self.data.get("error")
@@ -126,11 +132,14 @@ class Client():
         
         ext=self.data.get("ext",{})
         ack=str(ext.get("ack"))
-        print(ack)
 
         if ack!="True" and ack !="None":
+            if self.disconnected==True:
+                return
             self.pong_count=int(ack)
             self.pong()
+            if self.latest_listener_call=="question_awaited":
+                prev_func=self.listening_functions.get("question_started")
             func=self.listening_functions.get("pinged")
         elif ack=="True":
             self.clientId = self.data.get("clientId")
@@ -138,24 +147,42 @@ class Client():
             func=self.listening_functions.get("handshake1")
 
         id=data.get("id")
-        event_map={14:"handshake2",46:"avatar_updated",12:"disconnected",2:"question_started",8:"question_ended",9:"quiz_started",13:"quiz_ended"}
+        event_map={14:"handshake2",46:"avatar_updated",10:"disconnected",2:"question_awaited",8:"question_ended",9:"quiz_started",13:"quiz_ended"}
+        info=self.data
+        del info["ext"]
+        del info["channel"]
 
         if id in event_map:
+            if id==10:
+                if info.get("data",{}).get("content")=='{"kickCode":1}':
+                    del info["data"]["content"]
+                    info["reason"]="kicked"
+            if id ==2:
+                self.questionindex=int(JSON.loads(self.data.get("data").get("content")).get("gameBlockIndex"))
+                self.question_data=self.data
             func=self.listening_functions.get(event_map[id])
-            if id==2:
-                self.questionindex=JSON.loads(self.data.get("data").get("content")).get("gameBlockIndex")
+            self.latest_listener_call=event_map[id]
         elif data.get("reason")=="disconnect":
             func=self.listening_functions.get("disconnected")
+            info["reason"]='host left'
+            del info["data"]["reason"]
+            self.disconnected=True
+        elif str(data.get("type"))=="loginResponse":
+            self.latest_listener_call="joined"
+            func=self.listening_functions.get("joined")
         else:
             func=self.listening_functions.get("unknown message")
+            self.latest_listener_call="unknown message"
         
         if callable(func):
-            func(self.data)
+            func(info)
+        if callable(prev_func):
+            prev_func(self.previous_send)
+            self.latest_listener_call="question_answered"
     
     #used for sending messages
     def send(self, msg):
         try:
-            print(msg,"\n out \n")
             self.sent=msg[0]
             msg = JSON.dumps(msg)
             self.log.append(msg)
@@ -171,7 +198,8 @@ class Client():
 
     #used to respond to the kahoot servers pinging the client
     def pong(self):
-        self.send([{"id": str(self.message_count), "channel": self.WebChannels["META_CONNECT"], "clientId": str(self.clientId), "connectionType": "websocket", "ext": {"ack": self.pong_count, "timesync": {"tc": self.time(), "l": 0, "o": 0}}}])
+        packet=self.packet_factory("META_CONNECT",ext={"ack": self.pong_count, "timesync": {"tc": self.time(), "l": 0, "o": 0}})
+        self.send(packet)
         return "ponged"
 
     # used to generate the information on a profile
@@ -180,7 +208,7 @@ class Client():
         cosmetic_map = {'PANCAKES': 3950, 'CHRISTMAS_HAT': 5372, 'MONOCLE': 1250, 'SCARF': 2750, 'WINTER_HAT': 5371, 'EAR_MUFFS': 5370, 'SNOW_GOGGLES': 4100, 'COLORED_SUNGLASSES': 4050, 'SANTA_HAT': 5368, 'BEARD': 5367, 'TREE_HAT': 5366, 'PRESENT_HAT': 5365, 'KAHOOT_HAT': 1550, 'GLOWER_HAT': 3100, 'CROWN': 3150, 'VIKING_HAT': 3200, 'GRADUATION_CAP': 3250, 'COWBOY_HAT': 3300, 'WITCH_HAT': 3350, 'HEADPHONES': 3400, 'HEARTS': 3450, 'HEART_GLASSES': 3500, 'GOGGLES': 3550, 'HARD_HAT': 5300, 'EXPLORER_HAT': 5309, 'EYEPATCH': 3600, 'POWDERED_WIG': 1300, 'ALBERT_EINSTIEN': 1350, 'HAIR': 1400, 'SUNGLASSES': 3650, 'TOP_HAT': 3700, 'KID_HAT': 3750, 'PARTY_HAT': 3800, 'FAKE_DISGUISE': 3850, 'PACIFIER': 3900, 'ICE_CREAM_CONE': 4000, 'FOOTBALL_HELMET': 4150, 'ASTRONAUT_HELMET': 4200}
         avatarid=avatar_map.get(avatar.upper())
         cosmeticid=cosmetic_map.get(cosmetic.upper())
-        return JSON.dumps({"avatar":{"type":avatarid,"item":cosmeticid}})
+        return {"avatar":{"type":avatarid,"item":cosmeticid}}
 
     # kills the websocket if open
     def close(self):
@@ -198,10 +226,12 @@ class Client():
         self.name = name
         profile = JSON.dumps({"avatar":{"type":2050,"item":3350}}) if profile is None else profile
 
-        login_message = [{"id":str(self.message_count),"channel":self.WebChannels["SERVICE_CONTROLLER"],"data":{"type":"login","gameid":str(self.gamepin),"host":"kahoot.it","name":str(name),"content": JSON.dumps({"device":{"userAgent":f"{self.UserAgent}","screen":{"width":1920,"height":1080}}})},"clientId":str(self.clientId),"ext":{}}]
-        login_followup_message=[{"id":str(self.message_count+1),"channel":self.WebChannels["SERVICE_CONTROLLER"],"data":{"id":16,"type":"message","gameid":str(self.gamepin),"host":"kahoot.it","content":f"{profile}"},"clientId":str(self.clientId),"ext":{}}]
-        self.send(login_message)
-        self.send(login_followup_message)
+        login_data=self.data_factory({"device":{"userAgent":f"{self.UserAgent}","screen":{"width":1920,"height":1080}}},"login")
+        login_packet=self.packet_factory("SERVICE_CONTROLLER",login_data)
+        login_follup_data=self.data_factory(profile,"message",16)
+        login_followup_packet=self.packet_factory("SERVICE_CONTROLLER",login_follup_data)
+        self.send(login_packet)
+        self.send(login_followup_packet)
 
         time.sleep(1)
         self.logged_in.set()
@@ -209,23 +239,56 @@ class Client():
     
     # used to disconnect from a kahoot game
     def disconnect(self):
+        self.disconnected=True
         self.send([{"id":str(self.message_count),"channel":self.WebChannels["META_DISCONNECT"],"clientId":str(self.clientId),"ext":{"timesync":{"tc":self.time(),"l":0,"o":0}}}])
+        self.listening_functions["disconnected"]({"data":{"gameid":self.gamepin,'host':'kahoot.it',"id":10,'type':'message'},"reason":"player left"})
         self.logged_in.clear()
     
     # used to change profiles in a kahoot game
     def change_profile(self,profile):
         self.logged_in.wait()
-        self.send([{"id":str(self.message_count),"channel":self.WebChannels["SERVICE_CONTROLLER"],"data":{"id":46,"type":"message","gameid":str(self.gamepin),"host":"kahoot.it","content":f"{profile}"},"clientId":str(self.clientId),"ext":{}}])
+        data=self.data_factory(str(profile),"message",46)
+        packet=self.packet_factory("SERVICE_CONTROLLER",data)
+        self.send(packet)
 
     #used to submit an answer in a kahoot game
-    def submit_answer(self,answer):
-        time.sleep(random.random()+.5)
+    def submit_answer(self,answer,delay:int=0):
+        lag=round(random.random() * 60 + 5)
         answer=0 if answer=="red" else answer
         answer=1 if answer=="blue" else answer
         answer=2 if answer=="yellow" else answer
         answer=3 if answer=="green" else answer
-        self.send([{"channel": self.WebChannels["SERVICE_CONTROLLER"],"clientId": str(self.clientId),"data": {"content": JSON.dumps({"choice": answer,"questionIndex": self.questionindex,"meta": {"lag": round(random.random() * 45 + 5)},"type":"quiz"}),"gameid": str(self.gamepin),"host": "kahoot.it","id": 45,"type": "message"},"id": str(self.message_count)}])
+        time.sleep(delay+.25)
+        data=self.data_factory(JSON.dumps({"choice": answer, "questionIndex": self.questionindex, "meta": {"lag": lag}, "type": "quiz"}),"message",45)
+        packet=self.packet_factory("SERVICE_CONTROLLER",data)
+        self.send(packet)
         self.questionindex+=1
+
+    def packet_factory(self,channel,data="",ext={}):
+        packet=[{
+            "channel":self.WebChannels[channel],
+            "clientId":str(self.clientId),
+            "data":data,
+            "id":str(self.message_count),
+            "ext":ext,
+            "connectionType": "websocket"
+            }]
+        if data=="":
+            packet[0].pop("data")
+        return packet
+    
+    def data_factory(self, content, type1: str, id_int: int = -1):
+        data = {
+            "gameid": str(self.gamepin),
+            "host": "kahoot.it",
+            "type": str(type1),
+            "id": id_int,
+            "content": JSON.dumps(content),
+            "name": str(self.name)
+        }
+        if id_int==-1:
+            data.pop("id")
+        return data
 
     def random_answer(self):
         self.submit_answer(random.randint(0,3))
