0459738442dc93c51271290c2532f11856232fcf
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2023-12-23 22:20:58 -0700

    Update botsfromfile.py

--- examples/botsfromfile.py
+++ examples/botsfromfile.py
@@ -1,5 +1,4 @@
 from Pyhoot.Client import Client
-import time
 import threading
 
 botlist = []
@@ -8,23 +7,23 @@ with open("names.txt","r") as f:
   content=f.read()
   f.close()
 
-for i in content.split():
-   botlist.append(Client(gamepin=gamepin, name = i))
+for name in content.split():
+   botlist.append(Client(gamepin=gamepin, name = name))
+
+for bot in botlist:
+    @bot.event_listener("question_started")
+    def question(data):
+       bot.random_answer()
 
 def initBot():
-  for i in botlist:
-      i.start()
-      i.join()
-
-def answerBot():
-  for i in range(0,len(botlist)):
-      @botlist[i].event_listener("question_started")
-      def question(data):
-          for i in botlist:
-              i.random_answer()
+    for bot in botlist:
+        bot.start()
+        bot.join()
 
 
 thread1 = threading.Thread(target=initBot)
-thread2 = threading.Thread(target=answerBot)
 thread1.start()
-thread2.start()
+
+thread1.join()
+
+# warning this takes a while to initiate all the bots, might want to make multiple threads to starting at each end depending on file length
