e65eb5c6f42916857a41791c40b8e8124550bc18
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-01-03 12:15:03 -0700

    Update bots_from_file.py
    made same names auto deleted

--- examples/bots_from_file.py
+++ examples/bots_from_file.py
@@ -12,11 +12,20 @@ with open("names.txt", "r") as f:
     names = content.split()
     f.close()
 
+index=0
+for name in names:
+    names.pop(index)
+    if name in names:
+        names.remove(name)
+    names.append(name)
+    index+=1
+
 def create(name):
     bot_instances[name] = Client(gamepin=int(gamepin), name=name)
 
     @bot_instances[name].event_listener(type="question_started")
     def question(data):
+        time.sleep(random.randint(50,75)/100)
         bot_instances[name].random_answer()
 
     bot_instances[name].start()
@@ -27,7 +36,7 @@ for name in names:
     t = threading.Thread(target=create, args=(name,))
     t.start()
     threadlist.append(t)
-    time.sleep(random.randint(10,30)/10)
+    time.sleep(random.randint(10,20)/10)
 
 for thread in threadlist:
     thread.join()
