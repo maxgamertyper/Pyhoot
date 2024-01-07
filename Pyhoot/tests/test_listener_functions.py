c487a086270784ba6e0b579b02ef670abe5372c3
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-01-07 12:13:03 -0700

    Delete Pyhoot directory

--- Pyhoot/tests/test_listener_functions.py
+++
@@ -1,8 +0,0 @@
-from Pyhoot.Client import Client
-
-bot=Client()
-
-for listener_type in list(bot.ListeningData["types"].keys()):
-  @bot.event_listener(listening_type=listener_type)
-  def listen_test(data):
-    print(listener_type)
