3e019447f82f47a32d83c540c93d96413e4e53f0
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-05-23 22:26:26 -0600

    Update crashbot.py

--- examples/crashbot.py
+++ examples/crashbot.py
@@ -1,11 +1,1 @@
-from Pyhoot.Client import Client
-import time
-
-bot=Client(gamepin=str(input("gamepin: ")),name=str(input("name: ")),auth_bypass=True)
-
-@bot.event_listener(type="question_started")
-def question(data):
-    bot.crash_lobby()
-
-bot.start()
-bot.join()
+# will update in the future probably update V1.3.1
