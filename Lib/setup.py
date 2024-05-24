54eef05ab62b021b45c023561128476a4fcafa47
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-05-23 22:00:53 -0600

    Update setup.py

--- Lib/setup.py
+++ Lib/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.2.1'
+VERSION = '1.3.0'
 DESCRIPTION = 'A Python library used for the Kahoot! Api'
-LONG_DESCRIPTION = 'A package that allows users to create Kahoot! clients and use the Kahoot! API to their will.\nCheck out https://github.com/maxgamertyper/Pyhoot for more information.'
+LONG_DESCRIPTION = 'A package that allows users to create Kahoot! clients and use the Kahoot! API to their will.\nCheck out https://github.com/maxgamertyper/Pyhoot for more information. \n'
 
 
 setup(
@@ -14,7 +14,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['requests', 'beautifulsoup4', 'websocket-client', 'fake_useragent'],
-    keywords=['python', 'Kahoot', 'pyhoot', 'bot', 'flooder', 'sockets'],
+    keywords=['python', 'Kahoot', 'pyhoot', 'bot', 'flooder', 'Kahoot!'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
