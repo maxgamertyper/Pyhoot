80142e94a369f1e558a458b49419a054c12e68bc
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-06-09 13:25:40 -0600

    Update crashbot.py

--- examples/crashbot.py
+++ examples/crashbot.py
@@ -1,1 +1,14 @@
-# will update in the future probably update V1.3.1
+from Pyhoot.Client import Player
+
+bot=Player(Auth_Brute_Force=True,closeafterdisconnect=True)
+    
+@bot.event_listener(Ltype="question_started")
+def question(data):
+    bot.answer_crash() # or could do a profile_crash() somewhere else if wanted
+
+gameid=input("gamepin?")
+name=input("name?")
+bot.start(gameid)
+bot.join(name) # or bot.join_crash(name)
+
+# nothing works here after the WS is closed (working on fixing this)
