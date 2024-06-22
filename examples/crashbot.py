fe3be18a17ffb98681fd8657bc5dd425236f0819
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-06-21 18:56:40 -0600

    Update crashbot.py

--- examples/crashbot.py
+++ examples/crashbot.py
@@ -4,11 +4,11 @@ bot=Player(Auth_Brute_Force=True,closeafterdisconnect=True)
     
 @bot.event_listener(Ltype="question_started")
 def question(data):
-    bot.answer_crash() # or could do a profile_crash() somewhere else if wanted
+    bot.AnswerCrash() # or could do a profile_crash() somewhere else if wanted
 
 gameid=input("gamepin?")
 name=input("name?")
-bot.start(gameid)
-bot.join(name) # or bot.join_crash(name)
+bot.Start(gameid)
+bot.Join(name) # or bot.join_crash(name)
 
 # nothing works here after the WS is closed (working on fixing this)
