e43d7983946a4b53a0a737f87ec1626bbae1c822
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-06-26 14:17:48 -0600

    Update setup.py

--- Lib/setup.py
+++ Lib/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.4.0'
+VERSION = '1.4.1'
 DESCRIPTION = 'A Python library used for the Kahoot! Api'
 
 with open("./README.md", 'r') as rm:
